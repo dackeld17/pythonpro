f0a6d02bfac0cbb48b09b9a2748334a9fbe4f786
dackeld <dackeld@cu.ac.kr>
2023-10-04 01:26:25 +0000

    test

---
+++ chap4/prob3.py
@@ -0,0 +1,8 @@
+print("\t\t\tWelcome to 'Guess My Number'!")
+
+print("\nI'm thinking of a number between 1 and 100.")
+print("Try to guess it in as few ateempts as possible.")
+
+guess = Input("Take a guess: ")
+
+
