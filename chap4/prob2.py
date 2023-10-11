48bffd6d9cd48f5c4a2352fefe0de9ad6a0b75e7
dackeld <dackeld@cu.ac.kr>
2023-10-11 00:10:13 +0000

    test

--- chap4/prob2.py
+++ chap4/prob2.py
@@ -1,23 +1,46 @@
 import random
 
-herohp = random.randrange(50,100)
-monsterhp = random.randrange(50,100)
+hhp = random.randrange(50,100)
+mhp = random.randrange(50,100)
+num = 0
 
-value = random.randrange(-10,20)
-value2 = random.randrange(-10,20)
+print("hero HP:",hhp,",monster HP:",mhp)
 
-print("hero HP:",herohp,",","monster HP:",monsterhp)
-
-while(herohp >= 0 or monsterhp >= 0):
-
-    if(value <= 0):
-        attack = ('fail')
-    else :
-        attack = ('success')
-        herohp = herohp - value
-
-    if(value2 <= 0):
-        attack2 = ('fail')
-    else :
-        attack2 = ('success')
-print("hero(HP:",herohp,"attack:",value,")",attack) 
+while (hhp >= 0 and mhp >= 0):
+    matt = random.randrange(-10,20)
+    hatt = random.randrange(-10,20)
+    num += 1
+    if matt > 0:
+        hhp =hhp-matt
+        print("hero(HP:",hhp,", attack:",hatt,")",end ='')
+        if hatt > 0:
+            print("sucess <-> ",end ='')
+        else:
+            print("fail <-> ",end = '')
+    elif matt <=0:
+        print("hero(HP:",hhp,", attack:",hatt,")",end ='')
+        if hatt > 0 :
+            print("Success <-> ",end ='')
+        else :
+            print("fail <->", end ='')
+   
+    if hatt > 0:
+        mhp = mhp-hatt
+        print(" monster(HP:",mhp,", attack:",matt,")",end ='')
+        if matt > 0:
+            print("Success")
+        else:
+            print("Fail")
+    elif hatt <=0:
+        print(" monster(HP:",mhp,", attack:",matt,")",end ='')
+        if matt > 0:
+            print("success")
+        else:
+            print("fail")
+           
+print("--------------------------------------------\n")
+print("Total phase: ",num)
+if hhp <= 0:
+    print("Monster Win!!!")
+elif mhp <= 0:
+    print("Hero Win!!!")
