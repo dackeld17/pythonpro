4851bdf661c4d5f398fea346a293a927a56c7029
dackeld <dackeld@cu.ac.kr>
2023-10-11 00:02:06 +0000

    test

--- chap4/prob3.py
+++ chap4/prob3.py
@@ -1,8 +1,25 @@
+import random
+
+number = random.randint(1, 100)
+attempts = 0
+
 print("\t\t\tWelcome to 'Guess My Number'!")
 
 print("\nI'm thinking of a number between 1 and 100.")
-print("Try to guess it in as few ateempts as possible.")
+print("Try to guess it in as few ateempts as possible.\n")
 
-guess = Input("Take a guess: ")
+while True :
+        guess = int(input("Take a guess: "))
+        attempts += 1
 
+        if guess < number:
+            print("Higher...")
+        elif guess > number:
+            print("Lower...")
+        else:
+            print("You guessed it!. The number was",number)
+            print("And it only took you",attempts,"tries!")
+            break
+
+print("\n\nPress the enter key to quit.")
 
