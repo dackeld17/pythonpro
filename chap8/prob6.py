652d33ed43e161353ba830babbd4b1c81a32c106
dackeld <dackeld@cu.ac.kr>
2023-11-22 00:04:31 +0000

    .

---
+++ chap8/prob6.py
@@ -0,0 +1,33 @@
+import random
+import pickle
+
+def load_questions(file_path):
+    with open(file_path, 'rb') as file:
+        questions = pickle.load(file)
+    return questions
+
+def ask_question(question, options):
+    print(question)
+    for i in range(len(options)):
+        print(f"{i + 1}. {options[i]}")
+   
+    user_answer = input("Your answer : ")
+    return int(user_answer)
+
+def run_game(questions):
+    score = 0
+    random_question = random.choice(questions)
+    question, options, correct_answer = random_question
+   
+    user_answer = ask_question(question, options)
+    if user_answer == correct_answer:
+        print("\nCorrect!\n")
+        score += 1
+    else:
+        print(f"\nWrong! The correct answer was {correct_answer}\n")
+
+if __name__ == "__main__":
+    file_path = "trivia_data.pkl"
+    questions = load_questions(file_path)
+    run_game(questions)
+
